bf3697f48f3aceb35173f86edc84301234779569
Dali Dmitrii <dalidmitrii10@gmail.com>
2023-02-16 13:03:38 +0200

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramApi = require('node-telegram-bot-api');
 
-const token = '6296397052:AAFpmLACrObMltEKAQcBlFNHOg1Bhd8bDek';
+const token = process.env.TOKEN;
 
 const bot = new TelegramApi(token, {polling: true});
 
@@ -8,6 +8,6 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    await bot.sendMessage(chatId, 'Your message: ' + text);
+    await bot.sendMessage(chatId, `Your message: ${text} was received`);
 });
 
